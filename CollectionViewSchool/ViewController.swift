64ba9f82c6f6a164e7818e745aa27f1c9c84ff8c
Tobias Boogh <tobias@boogh.se>
2024-06-14 14:29:30 +0200

    Initial Commit

---
+++ CollectionViewSchool/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewSchool
+//
+//  Created by Tobias Boogh on 2024-06-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
